cd814dd4072e661399f3618efafd2aeec11f65e9
Malika Laouiti <laouiti.malika@yahoo.com>
2024-06-25 23:49:26 +0100

    fixing problem with styling v0

--- e-shop/src/pages/_app.tsx
+++ e-shop/src/pages/_app.tsx
@@ -1,9 +1,26 @@
-// src/pages/_app.js or src/pages/_app.tsx
+import React from 'react'
+import Head from 'next/head'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
 
-function MyApp({ Component, pageProps  }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component , pageProps } :AppProps) {
+  return (
+    <React.Fragment>
+      
+      <Head>
+          <title>eShop</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+      
+
+    </React.Fragment>
+  )
 }
 
-export default MyApp;
+export default MyApp
